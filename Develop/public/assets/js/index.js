d4877d28e7269f7acc185f9c452102d3dea1f5d2
Gabriela <gabriela.reyna89@icloud.com>
2023-04-11 22:41:01 -0400

    erased files on index.js i dint see they were there

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,8 +1,8 @@
-let noteTitle = document.querySelector('.note-title');
-let noteText = document.querySelector('.note-textarea');
-let saveNoteBtn = document.querySelector('.save-note');
-let newNoteBtn = document.querySelector('.new-note');
-let noteList = document.querySelector('.list-container .list-group');
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
